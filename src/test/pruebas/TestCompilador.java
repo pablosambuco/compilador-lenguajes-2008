898b2f6bc2e693509b3788c36c9b8889e9d5fe93
cioccio <cioccio@b04e956e-8985-11dd-abeb-270e0dc42671>
2008-09-23 13:59:30 +0000

    Cambio en la llamada al Test.

--- src/test/pruebas/TestCompilador.java
+++ src/test/pruebas/TestCompilador.java
@@ -5,7 +5,8 @@ import junit.framework.TestCase;
 public class TestCompilador extends TestCase {
 
 	public void testCompilador(){
-		Main.main(null);
+		String []argumentos = {new String("src/test/pruebas/programa.txt")};
+		Main.main(argumentos);
 	}
 
 }
