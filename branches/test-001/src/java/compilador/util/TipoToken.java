da167da293c753e88d537cd4f8256675cbaa4a8a
pncioccio <pncioccio@b04e956e-8985-11dd-abeb-270e0dc42671>
2008-09-24 20:20:09 +0000

    Ya tenemos el Yacc integrado! Iupiiiiii ;-)

--- branches/test-001/src/java/compilador/util/TipoToken.java
+++ branches/test-001/src/java/compilador/util/TipoToken.java
@@ -5,37 +5,39 @@ public class TipoToken {
 	/* TIPOS DE TOKEN */
 	/* NOTA: colocamos al principio aquellos que requieran tabla de simbolos */
 
-	public static final int ID 				= 0;
-	public static final int CTE_NUM 		= 1;
-	public static final int CTE_STR 		= 2;
-	public static final int OP_SUMA 		= 3;
-	public static final int OP_RESTA 		= 4;
-	public static final int OP_MUL 			= 5;
-	public static final int OP_DIV 			= 6;
-	public static final int OP_ASIG 		= 7;
-	public static final int AND 			= 8;
-	public static final int OR 				= 9;
-	public static final int OP_NEGACION 	= 10;
-	public static final int OP_IGUAL 		= 11;
-	public static final int OP_DISTINTO 	= 12;
-	public static final int OP_MAYOR 		= 13;
-	public static final int OP_MENOR 		= 14;
-	public static final int OP_MAYOR_IGUAL 	= 15;
-	public static final int OP_MENOR_IGUAL 	= 16;
-	public static final int PAR_ABRE 		= 17;
-	public static final int PAR_CIERRA 		= 18;
-	public static final int COR_ABRE 		= 19;
-	public static final int COR_CIERRA 		= 20;
-	public static final int COMA 			= 21;
-	public static final int DOS_PUNTOS 		= 22;
-	public static final int PUNTO_Y_COMA 	= 23;
+	public static final int ID 				= 257;
+	public static final int CTE_NUM 		= 258;
+	public static final int CTE_STR 		= 259;
+	public static final int OP_SUMA 		= 260;
+	public static final int OP_RESTA 		= 261;
+	public static final int OP_MUL 			= 262;
+	public static final int OP_DIV 			= 263;
+	public static final int OP_ASIG 		= 264;
+	public static final int AND 			= 265;
+	public static final int OR 				= 266;
+	public static final int OP_NEGACION 	= 267;
+	public static final int OP_IGUAL 		= 268;
+	public static final int OP_DISTINTO 	= 269;
+	public static final int OP_MAYOR 		= 270;
+	public static final int OP_MENOR 		= 271;
+	public static final int OP_MAYOR_IGUAL 	= 272;
+	public static final int OP_MENOR_IGUAL 	= 273;
+	public static final int PAR_ABRE 		= 274;
+	public static final int PAR_CIERRA 		= 275;
+	public static final int COR_ABRE 		= 276;
+	public static final int COR_CIERRA 		= 277;
+	public static final int COMA 			= 278;
+	public static final int DOS_PUNTOS 		= 279;
+	public static final int PUNTO_Y_COMA 	= 280;
 	
-	public static final int INCOMPLETO 		= 100;
+	public static final int INCOMPLETO 		= 0; //cuando el automata devuelva esto, yyparse() terminara de parsear ya que no estara recibiendo mas tokens
+
+	public static final int PR = 281;
+	
 	
 	/* En realidad este Tipo no se usa directamente, las palabras reservadas tienen cada una
 	 * su propio tipo; son consecutivos y empiezan en PR. 
 	 */
-	public static final int PR = 200;
 	
 	public static final int ERROR_LEXICO = -1;
 }
--- branches/test-001/src/test/pruebas/TestCompilador.java
+++ branches/test-001/src/test/pruebas/TestCompilador.java
@@ -1,12 +1,20 @@
 package pruebas;
 
+import compilador.parser.Parser;
+
 import junit.framework.TestCase;
 
 public class TestCompilador extends TestCase {
 
 	public void testCompilador(){
+
 		String []argumentos = {new String("src/test/pruebas/programa.txt")};
-		Main.main(argumentos);
+		
+		Parser.main(argumentos);
+		
+		//con esta línea se testea solo el analizador lexicografico
+		//Main.main(argumentos);
+		
 	}
 
 }
