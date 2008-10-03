babed82e4a83846320079ce16223da926c8c53b4
pablosambuco <pablosambuco@b04e956e-8985-11dd-abeb-270e0dc42671>
2008-10-03 21:51:39 +0000

    Buuuuu. No va a ser tan simple todo. Hay un pequeño problema pq todos los objetos $$, $1, $2, etc son static o algo asi. En casa veo...

--- src/java/make.bat
+++ src/java/make.bat
@@ -1,3 +1,8 @@
 @echo off
+del compilador\analizadorLexicografico\*.class
+del compilador\beans\*.class
+del compilador\parser\*.class
+del compilador\semantica\*.class
+del compilador\util\*.class
 javac -d . compilador\parser\Parser.java
 pause
