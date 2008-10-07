5ffe853bbce589b48f5783100e3851ce42bee501
pablosambuco <pablosambuco@b04e956e-8985-11dd-abeb-270e0dc42671>
2008-10-07 13:47:47 +0000

    Borro esto y copio lo de test-002 que anda bien. El trunk no andaba... :S

--- BYACC-J/generar.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-yacc.exe -J -Jpackage=compilador.parser input.y
-pause
-move *.java ..\src\java\compilador\parser
--- src/java/make.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-del compilador\analizadorLexicografico\*.class
-del compilador\beans\*.class
-del compilador\parser\*.class
-del compilador\semantica\*.class
-del compilador\util\*.class
-javac -d . compilador\parser\Parser.java
-pause
