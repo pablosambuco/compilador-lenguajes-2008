1ac30790d03d6a94214473f48d1d21a51b521971
pncioccio <pncioccio@b04e956e-8985-11dd-abeb-270e0dc42671>
2008-10-22 23:07:31 +0000

    Cambios a las clases Auxiliares

--- branches/test-003/src/java/compilador/sintaxis/VectorPolaca.java
+++ branches/test-003/src/java/compilador/sintaxis/VectorPolaca.java
@@ -1,5 +1,6 @@
 package compilador.sintaxis;
 
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.Vector;
 
@@ -13,19 +14,43 @@ public class VectorPolaca {
 			instance = new VectorPolaca();
 		return instance;
 	}
+	
 	private VectorPolaca() {
 		vector = new Vector<EntradaVectorPolaca>();
 	}
 	
 	public void agregar(EntradaVectorPolaca entrada) {
-		vector.add(vector.size(), entrada);
+		vector.add(entrada); //agrega al final del último elemento del vector
+	}
+	
+	public void agregar(EntradaVectorPolaca entrada, int posicion) {
+		/* Si se esta intentando insertar un elemento dejando huecos vacíos en el vector,
+		 * el mismo arrojará un error; por eso debemos llenar los espacios intermedios con "null"
+		 * (incluso si se lo quiere insertar en el primer hueco vacìo, ese también debe contener "null"
+		 * ya que el metodo "setElement" solo inserta en un casillero donde ya había algo */
+		 
+		if(posicion >= vector.size()) {
+			for(int x = posicion-vector.size(); x>=0; x--) {
+				vector.add(null);
+			}
+		} 
+		
+		vector.setElementAt(entrada, posicion); //agrega en el indice especificado reemplazando el objeto existente
+	}
+	
+	public void agregarCollection(Collection<EntradaVectorPolaca> collection) {
+		vector.addAll(collection);
 	}
 	
 	public void imprimirVector() {
 		Iterator<EntradaVectorPolaca> iterator = getInstance().vector.iterator();
 		while(iterator.hasNext()) {
-			System.out.println(iterator.next().toString() + " ");
+			System.out.print("|" + iterator.next());
 		}
+	}
+	
+	public int getPosicionActual() {
+		return vector.size();
 	}
 	
 }
